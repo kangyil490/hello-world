3d213582046b391843ec5f9b673b124970a57140
kangyil490 <79634090+kangyil490@users.noreply.github.com>
2021-02-25 03:17:08 -0700

    Update GitDemo.java

--- GitDemo.java
+++ GitDemo.java
@@ -30,7 +30,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber;
         return newNumber;
     }
     
@@ -47,7 +47,7 @@ public class GitDemo {
             factorialNumber = factorialNumber * 10 + temp;
             num /= 10;
         }
-        reverseNum = newNumber;
+        
         return factorialNumber;
     }
     
